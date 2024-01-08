15c7e277afbe060f1d9ed2cdb29d4a4c15e93521
Nilesh Suthar <102959052+nilesh-12@users.noreply.github.com>
2024-01-09 01:04:39 +0530

    feat: :sparkles: update quotes
    Whenever you divide your income, new quote will be shown

--- src/main.rs
+++ src/main.rs
@@ -8,39 +8,42 @@ const OPEXPER: f64 = 0.10;
 const RANDOM_INDICES: [usize;10] = [8,4,1,6,2,5,3,7,9,0];
 
 fn main() -> Result<(), slint::PlatformError> {
-    let current_random_quote_index = 0;
+    let mut current_random_quote_index = 0;
     let quotes = vec![
         String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!"),
-        String::from("Keep filling even it's overflowing!")
+        String::from("Let Go!"),
+        String::from("Enough!"),
+        String::from("Break Free, Grow!"),
+        String::from("Rage, Destroy, Finish!"),
+        String::from("Confident!"),
+        String::from("Better tomorrow!"),
+        String::from("Kneel!"),
+        String::from("Bear It 'Til You Can't!"),
+        String::from("Just Begin!")
     ];
     let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
 
-    match quotes.get(RANDOM_INDICES[current_random_quote_index]) {
-        Some(quote) => {
-            ui.set_label(quote.into());
-        },
-        None => println!("There is no quote!"),
-    }
-
-    ui.on_divide_income(move |income| {
+    ui.on_divide_income( move |income| {
         let ui = ui_handle.unwrap();
         let num: f64 = income.trim().parse().unwrap();
         let tax: f64 = num * TAXPER;
         let owner: f64 = num * OWNERPER;
         let profit: f64 = num * PROFITPER;
         let opex: f64 = num * OPEXPER;
-        ui.set_results(format_results(tax, owner, profit, opex).into())
+        ui.set_results(format_results(tax, owner, profit, opex).into());
+        let value = ui.get_slider_value();
+        match quotes.get(RANDOM_INDICES[current_random_quote_index]) {
+            Some(quote) => {
+                ui.set_quote(quote.into());
+            },
+            None => (),
+        }
+        current_random_quote_index += 1;
+        if current_random_quote_index >= RANDOM_INDICES.len() {
+            current_random_quote_index = 0;
+        }
     });
-
     ui.run()
 }
 
