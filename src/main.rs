36fd3d6dae96a97e0a6da09de12b2839e304d099
Nilesh Suthar <102959052+nilesh-12@users.noreply.github.com>
2024-01-08 22:23:22 +0530

    feat: :technologist: add curtain animation
    animation for results and random quote selection

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,32 @@ const OWNERPER: f64 = 0.55;
 const PROFITPER: f64 = 0.05;
 const OPEXPER: f64 = 0.10;
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+const RANDOM_INDICES: [usize;10] = [8,4,1,6,2,5,3,7,9,0];
 
+fn main() -> Result<(), slint::PlatformError> {
+    let current_random_quote_index = 0;
+    let quotes = vec![
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!"),
+        String::from("Keep filling even it's overflowing!")
+    ];
+    let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
-    ui.set_label("Keep filling even it's overflowing!".into());
+
+    match quotes.get(RANDOM_INDICES[current_random_quote_index]) {
+        Some(quote) => {
+            ui.set_label(quote.into());
+        },
+        None => println!("There is no quote!"),
+    }
+
     ui.on_divide_income(move |income| {
         let ui = ui_handle.unwrap();
         let num: f64 = income.trim().parse().unwrap();
